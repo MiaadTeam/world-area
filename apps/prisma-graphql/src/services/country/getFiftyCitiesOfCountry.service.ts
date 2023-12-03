3de603703731cb7117789163714ef5dc7825adb2
emirom <meyari73@gmail.com>
2023-12-03 10:32:36 +0330

    feat: add timing for prisma getFiftyCities service

--- apps/prisma-graphql/src/services/country/getFiftyCitiesOfCountry.service.ts
+++ apps/prisma-graphql/src/services/country/getFiftyCitiesOfCountry.service.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 const getFiftyCitiesOfCountryService = async (limit = 50, pageNumber = 1) => {
+	const before = Date.now()
+	
 	const countries= await prisma.country.findMany({
 		select: {
 			name: true,
@@ -32,6 +34,11 @@ const getFiftyCitiesOfCountryService = async (limit = 50, pageNumber = 1) => {
 		}
 	
 	})
+
+	const after = Date.now()
+	const elapseTime = after - before
+	console.log("Elapsed Time: ", elapseTime)
+
 	return countries
 }
 
